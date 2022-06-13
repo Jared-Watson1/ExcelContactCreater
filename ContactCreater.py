47b553a31f36ab00df397d425d09bd03f0bbf59e
watsojar <jared.watson@emory.edu>
2022-06-13 15:26:08 -0400

    Configured code to handle different order of headers
    Code can correspond the headers to map to correct fields no matter the order in which the headers appear in the input file.
    
    Also added another label to the GUI to display instructions for the required fields for the user to input.

--- ContactCreater.py
+++ ContactCreater.py
@@ -30,16 +30,33 @@ class Contact:
         self.lastName = lastName
         # self.fullName = f"{self.firstName} {self.lastName}"
         self.company = company
+
         self.zip = zip
+        self.county = self.getCounty()
+        self.state = self.getState()
+        self.city = self.getCity()
+
         self.mobileNumber = mobileNumber
-        self.personas = personas 
+        # self.personas = self.matchPersona(personas) 
+        self.personas = personas
         self.industry = industry 
-        self.email = email 
+        self.email = email.lower() 
         self.jobTitle = jobTitle 
         self.revenue = revenue 
+        
+
+    def matchPersona(self, persona):
+        # match persona type inputted by registrant with hubspot predefinded personas
+        personaTypes = ["Entrepreneur/Founder", "Talent", "Investor", "Governement", "Employee", 
+                        "Innovation Enabler", "Higher Education"]
+        for p in personaTypes:
+            if str(persona) in p:
+                return p
+        
+        return persona
 
     def getCounty(self):
-        # obtain registrant county data from user inputted zipcode
+        # uses zip code to find regitrants county
         search = uszipcode.SearchEngine()
         try:
             zipcode = search.by_zipcode(self.zip)
@@ -48,6 +65,7 @@ class Contact:
             return "Invalid zip: no county"
 
     def getCity(self):
+        # uses zip code to find city
         search = uszipcode.SearchEngine()
         try:
             zipcode = search.by_zipcode(self.zip)
@@ -56,6 +74,7 @@ class Contact:
             return "Invalid zip"
 
     def getState(self):
+        # uses zip code to find state
         search = uszipcode.SearchEngine()
         try:
             zipcode = search.by_zipcode(self.zip)
@@ -64,9 +83,10 @@ class Contact:
             return -1
 
     def toDict(self):
-        contactDict = {"firstName": self.firstName, "lastName": self.lastName, "company": self.company, "zip": self.zip, "county": self.getCounty(), 
+        # seperate registrant information into dict data structure
+        contactDict = {"firstName": self.firstName, "lastName": self.lastName, "company": self.company, "zip": self.zip, "county": self.county, 
         "phone number": self.mobileNumber, "personas": self.personas, "industry": self.industry, "email": self.email, 
-        "job title": self.jobTitle, "revenue": self.revenue}
+        "job title": self.jobTitle, "revenue": self.revenue, "state": self.state, "city": self.city}
 
         return contactDict
 
@@ -83,19 +103,24 @@ class xlToContact:
         file = openpyxl.load_workbook(self.file)
         sheet = file.active 
 
-        for row in sheet.iter_rows():
-            name = (row[0].value, row[1].value) # firstname, lastname
-            # print(name)
-            company = row[2].value
-            zip = row[5].value
-            number = row[6].value
-            personas = row[7].value
-            industry = row[9].value
-            email = row[10].value
-            jobTitle = row[11].value
-            revenue = row[13].value
+        headerNames = self.getHeaders(sheet)
+        # print(headerNames)
 
-            newContact = Contact(name[0], name[1], company, zip, number, personas, industry, 
+        for row in sheet.iter_rows():
+            # obtain registrant properties
+            firstName = row[headerNames.get("First Name")].value
+            lastName = row[headerNames.get("Last Name")].value
+            # print(name)
+            company = row[headerNames.get("Company")].value
+            zip = row[headerNames.get("Zip Code")].value
+            number = row[headerNames.get("Mobile number")].value
+            personas = row[headerNames.get("Personas")].value
+            industry = row[headerNames.get("Industry Selection")].value
+            email = row[headerNames.get("Email Address")].value
+            jobTitle = row[headerNames.get("Job Title")].value
+            revenue = row[headerNames.get("Revenue")].value
+
+            newContact = Contact(firstName, lastName, company, zip, number, personas, industry, 
             email, jobTitle, revenue)
 
             self.contacts.append(newContact)
@@ -108,11 +133,28 @@ class xlToContact:
             else:
                 self.regions[county].append(data)
 
+    def getHeaders(self, sheet):
+        # get headers and their location from inputted .xlsx file 
+        # pass in the active sheet as parameter and loop through 1st column and map index of header to head in dict
+        # by default header location set to index 0
+        headers = {"First Name": 0, "Last Name": 0, "Job Title": 0, "Company": 0, "Company Website": 0, "Country Industry": 0, 
+        "Zip Code": 0, "Mobile number": 0, "Personas": 0, "Industry Selection": 0, "Revenue": 0, "Email Address": 0}
+
+        value = 0
+        for cell in sheet[1]:
+            cell = cell.value
+            headers[cell] = value 
+            value += 1
+
+        return headers
+
     def toJson(self, file="ContactsByRegion.json"):
+        # because registrant data is mapped to a dict then data can easily be made into .json form
         with open(file, 'w') as f:
             json.dump(self.regions, f, indent=4)
 
     def toExcel(self, file="Output.xlsx"):
+        # create .xlsx file of all registrants with added regional information
         for contact in self.contacts:
             self.data.append(contact.toDict())
         df = pd.DataFrame.from_dict(self.data)
@@ -124,6 +166,9 @@ class xlToContact:
 
 
     def sortByCounty(self, file="ContactsByRegion.xlsx"):
+        # sort registrants by region
+        # map all registrants to dictionary containing all counties
+        # loop through each county in dict and outputted order is sorted by county
         for region in self.regions:
             for contact in self.regions[region]:
                 self.sortedDataByRegion.append(contact)
@@ -149,6 +194,7 @@ class xlToContact:
 
 
 def tester(file="test.xlsx"):
+    # tester function
     x = xlToContact(file=file)
     x.toExcel()
     x.sortByCounty()
@@ -185,8 +231,6 @@ def main():
 # tester()
 
 
-
-
 from tkinter import *
 from tkinter.ttk import *
 from tkinter.filedialog import askopenfile 
@@ -196,7 +240,7 @@ def GUI():
 
     ws = Tk()
     ws.title('Bizzabo --> Hubspot')
-    ws.geometry('500x300') 
+    ws.geometry('700x400') 
 
     global inputFile
     def open_file():
@@ -251,6 +295,12 @@ def GUI():
         command=uploadFiles
         )
     upld.grid(row=3, columnspan=3, pady=10)
+
+    instructions = Label(
+        ws,
+        text="Download contact list from bizzabo with the following properties:\n     First Name\n     Last Name\n     Company\n     Zip Code\n     County\n     Mobile Number\n     Personas\n     Industry\n     Email\n     Job Title\n     Employees\n     Revenue"
+    )
+    instructions.grid(row=4, column=0)
 
 
     ws.mainloop()
